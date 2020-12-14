8cd4060ea678c62781b83697955e4754acc28b64
Blake <blake.cs.ml@gmail.com>
2020-12-13 19:57:44 -0500

    R comments

--- R/main.R
+++ R/main.R
@@ -6,6 +6,7 @@
 # delta_E:	"Change in energy"
 # E:		"Energy"
 # J:    	"Nearest-Neighbor Interactions"
+# H:        "Cost"
 # K:    	"Average number of neighbors per site (point)"
 # m:    	"Magnetization"
 # mu:   	"Spin Value"
@@ -13,6 +14,9 @@
 # P:		"Probability of bond between neighboring spins"
 # q:    	"Number of states (or classes or clusters)"
 # T:		"Temperature"
+# T_fs:     "Superparamagnetic state caused by pseudotransition from ferromagnetic state (initial peak)"
+# T_ps:     "Paramagnetic state caused by pseudotransition from superparamagnetic state (final valley)"
+# T_clus:   "Superparamagnetic temperature at which clusters form ((T_fs + T_ps)/2)"
 
 
 
